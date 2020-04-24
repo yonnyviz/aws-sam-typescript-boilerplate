e44f78980a9eb48bfb90259169b6d4ec54c36ae2
Yonny Vizcaya <yonnyviz@Yonnys-MacBook-Pro.local>
2020-04-23 20:27:55 -0500

    Configuring Proyect

---
+++ curl-request.sh
@@ -0,0 +1,4 @@
+curl --header "Content-Type: application/json" \
+    --request POST \
+    -d @./event.json \
+    http://localhost:3000/hello
--- src/hello.ts
+++ src/hello.ts
@@ -9,6 +9,7 @@ export interface Event {
 // This example demonstrates a NodeJS 8.10 async handler[1], however of course you could use
 // the more traditional callback-style handler.
 // [1]: https://aws.amazon.com/blogs/compute/node-js-8-10-runtime-now-available-in-aws-lambda/
-export default async (event: Event): Promise<string> => (
-  `Hello ${event.name}`
+export default async (event: any): Promise<any> => (
+  event.name
+  // `Hello ${event.name}`
 );
